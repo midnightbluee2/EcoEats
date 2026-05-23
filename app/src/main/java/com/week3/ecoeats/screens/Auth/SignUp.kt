c4fde91d9ab581698a151ec1cc7bb86994f3079f
midnightbluee2 <alenamashia@gmail.com>
2026-05-23 12:31:50 +0700

    Add : Fitur baru Sign Up dan Navigationnya

---
+++ app/src/main/java/com/week3/ecoeats/screens/Auth/SignUp.kt
@@ -0,0 +1,150 @@
+package com.week3.ecoeats.screens.Auth
+
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.week3.ecoeats.ui.theme.DarkOliveGreen
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import com.week3.ecoeats.R
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import com.week3.ecoeats.screens.components.MainImage
+import com.week3.ecoeats.ui.theme.CharcoalBrown
+import com.week3.ecoeats.ui.theme.Cornsilk
+import com.week3.ecoeats.ui.theme.LaurelGreen
+import com.week3.ecoeats.ui.theme.white
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.Dp
+import androidx.navigation.NavController
+
+
+@Composable
+fun SignUp(navController: NavController,modifier: Modifier = Modifier) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Cornsilk)
+    ) {
+        MainImage()
+        BottomLayer(
+            email = email,
+            onEmailChange = { email = it },
+            password = password,
+            onPasswordChange = { password = it },
+            modifier = Modifier
+                .fillMaxHeight (0.62f)
+                .align(Alignment.BottomCenter)
+        )
+    }
+}
+
+@Composable
+fun headline() {
+    Text(
+        text = "DAFTAR",
+        color = DarkOliveGreen,
+        fontSize = 22.sp,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center,
+        lineHeight = 32.sp
+    )
+}
+
+@Composable
+fun FieldEmail(value: String, onValueChange: (String) -> Unit) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        placeholder = { Text("Email") },
+        modifier = Modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(50),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+        singleLine = true
+    )
+}
+
+@Composable
+fun FieldPassword(value: String, onValueChange: (String) -> Unit) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        placeholder = { Text("Password") },
+        modifier = Modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(50),
+        visualTransformation = PasswordVisualTransformation(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        singleLine = true
+    )
+}
+
+@Composable
+fun BottomLayer(
+    email: String,
+    onEmailChange: (String) -> Unit,
+    password: String,
+    onPasswordChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(topStart = 32.dp, topEnd = 32.dp))
+            .background(Cornsilk)
+            .padding(horizontal = 32.dp, vertical = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(36.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.height(16.dp))
+        headline()
+
+        Column(modifier = Modifier.fillMaxWidth()) {
+            FieldEmail(value = email, onValueChange = onEmailChange)
+            Spacer(modifier = Modifier.height(32.dp))
+            FieldPassword(value = password, onValueChange = onPasswordChange)
+            Spacer(modifier = Modifier.height(90.dp))
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(56.dp),
+                shape = MaterialTheme.shapes.large,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = DarkOliveGreen,
+                    contentColor = white)
+                ) {
+                    Text(text = "Daftar")
+                }
+        }
+    }
+}
